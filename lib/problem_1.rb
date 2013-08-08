991a55a090b66d7989a3a84f9ecfc36d89f75614
jwilliams <jwilliams722@gmail.com>
2013-08-08 17:11:43 -0600

    Sum multiples of 3 or 5
    * Created method to find multiples of 3
      or 5 when passed a number
    
    * Created method to sum those multiples and
      return value

--- lib/problem_1.rb
+++ lib/problem_1.rb
@@ -1,3 +1,16 @@
 class Problem1
 
+  def find_multiples_of(number)
+    (1...number).to_a.select { |number| number % 3 == 0 || number % 5 == 0 }
+    # select - loops through array, if it returns true include it in the new array if false don't include
+    # reject does the opposite
+  end
+
+  def sums_multiples_of(number)
+    find_multiples_of(number).inject(0) { |result, element| result + element }
+    # start with 0 and pass that in with result, 0 + 3 = 3, 3 is now result, result is always the accumulation of what was added in the
+    # last loop, 3 + 5 = 8, 8 is now the result, and so on until the array is looped entirely, then it returns the last result
+    # if you start with an array of arrays of usernames and number of comments and want to create a hash with usernames as the key
+    # and the amount of comments they have as the value
+  end
 end
--- spec/lib/problem_1_spec.rb
+++ spec/lib/problem_1_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe Problem1 do
+  describe ".find_multiples_of(number)" do
+    it "returns 3, 5, 6, 9 when passed 10" do
+      multiples = [3, 5, 6, 9]
+      expect(subject.find_multiples_of(10)).to eq(multiples)
+    end
+  end
 
+  describe ".sums_multiples_of(number)" do
+    it "returns 23 when passed 10" do
+      expect(subject.sums_multiples_of(10)).to eq(23)
+    end
+
+    it "returns 233168 when passed 1000" do
+      expect(subject.sums_multiples_of(1000)).to eq(233168)
+    end
+  end
 end
